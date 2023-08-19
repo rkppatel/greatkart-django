a037fafb9c3790c39bd09169d034253aec5a761d
rakesh <rkppatel27@gmail.com>
2023-08-19 15:39:21 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -135,3 +135,23 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+    
+}
+
+
+# SMTP CONFIGURATION - EMAIL
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'rakeshsls333@gmail.com'
+EMAIL_HOST_PASSWORD = 'cqnljvecikpkodds'
+EMAIL_USE_TLS = True
+
+#cqnljvecikpkodds
+ 
